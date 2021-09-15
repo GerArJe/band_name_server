3758c358235043e1ff8e6164ee2bd98a873794fc
German <germanarevalojerez@gmail.com>
2021-09-15 10:46:29 -0500

    events

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,14 +1,19 @@
-const {io} = require('../index');
+const { io } = require('../index');
 
 //Socket messages
 io.on('connection', (client) => {
-    console.log('Client connected');
-    client.on('disconnect', () => {
-      console.log('Client disconnected');
-    });
-  
-    client.on('message', (payload) => {
-      console.log('message', payload);
-      io.emit('message', {admin: 'New message'});
-    });
+  console.log('Client connected');
+  client.on('disconnect', () => {
+    console.log('Client disconnected');
   });
+
+  client.on('message', (payload) => {
+    console.log('message', payload);
+    io.emit('message', { admin: 'New message' });
+  });
+
+  client.on('new-message', (payload) => {
+    // io.emit('new-message', payload);
+    client.broadcast.emit('new-message', payload);
+  });
+});
