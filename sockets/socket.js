786d64e8f1d2cad37948a5f9f9484f0f7527ce1d
German <germanarevalojerez@gmail.com>
2021-09-16 10:21:40 -0500

    emit bands

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,20 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon jovi'));
+bands.addBand(new Band('Heores del silencio'));
+bands.addBand(new Band('Metalica'));
 
 //Socket messages
 io.on('connection', (client) => {
   console.log('Client connected');
+
+  client.emit('active-bands', bands.getBands());
+
   client.on('disconnect', () => {
     console.log('Client disconnected');
   });
